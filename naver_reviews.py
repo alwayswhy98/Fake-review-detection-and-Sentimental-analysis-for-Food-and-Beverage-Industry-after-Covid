d86e5e28596e34b51ff8d0fa604c0b4daca197b2
alwayswhy98 <choima989@gmail.com>
2022-07-20 22:19:06 +0800

    Scrap reviews and save to csv (hard-coded)

--- naver_reviews.py
+++ naver_reviews.py
@@ -34,65 +34,58 @@ class naver_review():
         review_btn = brower.find_element(By.XPATH, '//*[@id="content"]/div/div[3]/div[3]/ul/li[2]/a')
         review_btn.click()
 
+
+
         page_cnt = 1
+        flag = True
 
         #Gather reviews
-        """
-        reviews = brower.find_elements(By.CLASS_NAME,"_2389dRohZq")
-        print("len reviews: ", len(reviews))
-        for review in reviews:
-            #print(review)
-            html_code = review.get_attribute("innerHTML")
-            html_code = html_code.replace("\n", " ")
-            #print(html_code)
-            rating_result = re.search('(.*)<em class="_15NU42F3kT">(.*)</em>(.*)', html_code)
-            if rating_result:
-                rating = rating_result.group(2)
-                ratings.append(rating)
-            else:
-                ratings.append(np.nan)
-                #print(rating)
-            date_result = re.search('(.*)</strong><span class="_3QDEeS6NLn">(.*)</span><div class="_1JZCQjfNPR">(.*)', html_code)
-            if date_result:
-                date = date_result.group(2)
-                dates.append(date[:8])
-            else:
-                dates.append(np.nan)
-                #print(date)
-            title_result = re.search('(.*)<button class="_3jZQShkMWY" aria-expanded="false" disabled=""><span class="_3QDEeS6NLn">(.*)</span></button></div></div></div></div><div class="_19SE1Dnqkf">(.*)', html_code)
-            if title_result:
-                title = title_result.group(2)
-                titles.append(title)
-            else:
-                titles.append(np.nan)
-                #print(title)
-            content_result = re.search('(.*)</span></button></div></div></div></div><div class="_19SE1Dnqkf"><div class="YEtwtZFLDz vlXMQEAtPR"><span class="_3QDEeS6NLn">(.*)</span></div>(.*)<div class="Ogt5tXwmlq">(.*)', html_code)
-            if content_result:
-                content = content_result.group(2)
-                contents.append(content)
-            else:
-                contents.append(np.nan)
-                #print(content)
-            helpful_result = re.search('(.*)<span class="count">(.*)</span></button>(.*)', html_code)
-            if helpful_result:
-                helpful = helpful_result.group(2)
-                helpfuls.append(helpful[0])
-            else:
-                helpfuls.append(np.nan)
-                #print(helpful)
+        while flag:
+            reviews = brower.find_elements(By.CLASS_NAME,"_2389dRohZq")
+            print("len reviews: ", len(reviews))
+            for review in reviews:
+                #print(review)
+                html_code = review.get_attribute("innerHTML")
+                html_code = html_code.replace("\n", " ")
+                #print(html_code)
+                rating_result = re.search('(.*)<em class="_15NU42F3kT">(.*)</em>(.*)', html_code)
+                if rating_result:
+                    rating = rating_result.group(2)
+                    ratings.append(rating)
+                else:
+                    ratings.append(np.nan)
+                    #print(rating)
+                date_result = re.search('(.*)</strong><span class="_3QDEeS6NLn">(.*)</span><div class="_1JZCQjfNPR">(.*)', html_code)
+                if date_result:
+                    date = date_result.group(2)
+                    dates.append(date[:8])
+                else:
+                    dates.append(np.nan)
+                    #print(date)
+                title_result = re.search('(.*)<button class="_3jZQShkMWY" aria-expanded="false" disabled=""><span class="_3QDEeS6NLn">(.*)</span></button></div></div></div></div><div class="_19SE1Dnqkf">(.*)', html_code)
+                if title_result:
+                    title = title_result.group(2)
+                    titles.append(title)
+                else:
+                    titles.append(np.nan)
+                    #print(title)
+                content_result = re.search('(.*)</span></button></div></div></div></div><div class="_19SE1Dnqkf"><div class="YEtwtZFLDz vlXMQEAtPR"><span class="_3QDEeS6NLn">(.*)</span></div>(.*)<div class="Ogt5tXwmlq">(.*)', html_code)
+                if content_result:
+                    content = content_result.group(2)
+                    contents.append(content)
+                else:
+                    contents.append(np.nan)
+                    #print(content)
+                helpful_result = re.search('(.*)<span class="count">(.*)</span></button>(.*)', html_code)
+                if helpful_result:
+                    helpful = helpful_result.group(2)
+                    helpfuls.append(helpful[0])
+                else:
+                    helpfuls.append(np.nan)
+                    #print(helpful)
 
-            time.sleep(2)
-        
+                time.sleep(2)
 
-        print(len(rating))
-        print("###########################################################")
-        print(ratings)
-        print("###########################################################")
-        print(dates)
-        print("###########################################################")
-        print(helpfuls)
-        """
-        for _ in range(27):
             page_cnt += 1
             print(page_cnt)
             if page_cnt % 10 != 1:
@@ -101,17 +94,31 @@ class naver_review():
                     page_btn = brower.find_element(By.XPATH, f'//*[@id="REVIEW"]/div/div[3]/div/div[2]/div/div/a[{page_num}]')
                 else:
                     page_btn = brower.find_element(By.XPATH, f'//*[@id="REVIEW"]/div/div[3]/div/div[2]/div/div/a[11]')
-                
+                    
                 try:
                     page_btn.click()
                 except:
+                    flag = False
                     print("end page")
-                    
+
             else:
                 page_btn = brower.find_element(By.XPATH, '//*[@id="REVIEW"]/div/div[3]/div/div[2]/div/div/a[12]')
                 page_btn.click()
 
             time.sleep(3)
+
+        data = {
+            "rating": ratings,
+            "date": dates,
+            "title": titles,
+            "content": contents,
+            "helpful count": helpfuls
+        }
+
+        df = pd.DataFrame(data)
+        df.to_csv("naver_reviews.csv")
+
+
 
 naver  = naver_review()
 
